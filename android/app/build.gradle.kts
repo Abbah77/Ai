23547a9d99a6147d824d6920a12e01b7b84ace68
GitHub Action <action@github.com>
2026-01-23 03:57:50 +0000

    Repair Android folder structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ai_project"
+    namespace = "com.example.here"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ai_project"
+        applicationId = "com.example.here"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/here/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/here/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ai_project
+package com.example.here
 
 import io.flutter.embedding.android.FlutterActivity
 
